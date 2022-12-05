5163538eabe14c26134cd2d5d862d3142c6ba56f
kimsuubin <jkl3435@naver.com>
2022-12-05 15:19:24 +0900

    Updates

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WrapComponenet from './components/WrapComponenet.jsx';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <WrapComponenet/>
-);
